89d09e476bbc211f3fa1174b31121e66e82c8b6f
akgmarquez <akgmarquez@gmail.com>
2018-07-10 22:53:19 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -11,6 +11,9 @@ end
 
 def a_few_more_steps
   # Write a loop that outputs the first two sets of steps in the Two-Step
+puts "Right foot steps right and back"
+puts "Left foot crosses over right"
+puts "Right foor steps right"
 end
 
 def how_many_steps?
